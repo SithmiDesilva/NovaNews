403328f1975921c382fe6bb03fcd8fd34fb5a8e6
Sithmi Desilva <sithmi.20231045@iit.ac.lk>
2026-04-15 16:41:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes.news import router as news_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -8,3 +10,11 @@ app.include_router(news_router)
 @app.get("/")
 def home():
     return {"message": "NovaNews RAG System Running"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
